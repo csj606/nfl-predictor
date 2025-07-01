a3900ca30949f58571515451461abbe66de948c8
csj606 <139289685+csj606@users.noreply.github.com>
2025-06-30 22:10:22 -0500

    Rewrote login components
    Made JSX components for the login and registration pages more dynamic and simpler to maintain

--- Frontend/nfl-frontend/src/LoginPage.tsx
+++ Frontend/nfl-frontend/src/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import dotenv from 'dotenv'
+import {team_names} from './constants.tsx'
 dotenv.config()
 const backendURL = import.meta.env.VITE_BACKEND_URL
 
@@ -40,20 +41,53 @@ function loginForm(){
 }
 
 function registerForm(){
-    return <form>
-        <label>First Name:</label>
-        <input id="first_name" type="text"></input>
-        <label>Last Name:</label>
-        <input id="last_name" type="text"></input>
+    const [first_name, changeFirstName] = useState("")
+    const [last_name, changeLastName] = useState("")
+    const [email, changeEmail] = useState("")
+    const [username, changeUsername] = useState("")
+    const [password, changePassword] = useState("")
+    const [favoriteTeam, changeFavoriteTeam] = useState("")
+
+    const submission = async (e: any) => {
+        e.preventDefault()
+        await fetch(`${backendURL}/register`, {
+            method: "POST",
+            body: JSON.stringify({
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                username: username,
+                favoriteTeam: favoriteTeam
+            })
+        }).then(
+            (res) => {
+                if(res.status == 401){
+                    setMessage("Error on user registration")
+                }
+            }
+        ).catch(
+
+        )
+    }
+
+    return <form onSubmit = {submission}>
+        <label htmlFor='first_name'>First Name:</label>
+        <input id="first_name" type="text" value={first_name} onChange={(e) => changeFirstName(e.target.value)}></input>
+        <label htmlFor='last_name'>Last Name:</label>
+        <input id="last_name" type="text" value={last_name} onChange={(e) => changeLastName(e.target.value)}></input>
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email"></input>
-        <label>Username:</label>
-        <input></input>
-        <label>Password:</label>
-        <input></input>
-        <label>Select your favorite team!</label>
-        <select id="team_options">
-            <option value=""></option>
+        <input id="email" type="email" value={email} onChange={(e) => changeEmail(e.target.value)}></input>
+        <label htmlFor='username'>Username:</label>
+        <input id='username' value={username} onChange={(e) => changeUsername(e.target.value)}></input>
+        <label htmlFor='password'>Password:</label>
+        <input id ='password' value={password} onChange={(e) => changePassword(e.target.value)}></input>
+        <label htmlFor='team_options'>Select your favorite team!</label>
+        <select id="team_options" onChange={(e) => changeFavoriteTeam(e.target.value)}>
+            {
+                team_names.map((item: string) => (
+                    <option key={item} value={item}></option>
+                ))
+            }
         </select>
     </form>
 }
