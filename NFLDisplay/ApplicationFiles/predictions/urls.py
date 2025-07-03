79654a1ca1340bfa283ec4379e6cf0be05611ae0
csj606 <139289685+csj606@users.noreply.github.com>
2025-07-02 23:27:39 -0500

    View Stubs + Auth System
    Created the initial view stubs and set up the authentication system for Django. Went ahead and rewrote the Navigation bar component as well.

--- NFLDisplay/ApplicationFiles/predictions/urls.py
+++ NFLDisplay/ApplicationFiles/predictions/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
--- NFLDisplay/ApplicationFiles/predictions/views.py
+++ NFLDisplay/ApplicationFiles/predictions/views.py
@@ -1,8 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+
 
 # Create your views here.
 
 
-def authenticate():
-    
+def home(request):
+    #TODO
+    return render(request, "predictions/home.html")
+
+
+def teams(request):
+    #TODO
+    return render(request, "predictions/home.html")
+
+
+def challenges(request):
+    #TODO
+    return render(request, "predictions/home.html")
+
+
+def rankings(request):
+    #TODO
+    return render(request, "predictions/home.html")
+
+
+@login_required
+def league(request):
+    #TODO
+    return render(request, "predictions/home.html")
