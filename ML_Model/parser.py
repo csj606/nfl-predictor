045caf8281aabcf7128c402ef92d503e33a45c99
csj606 <139289685+csj606@users.noreply.github.com>
2025-07-05 21:32:55 -0500

    Pandas schemas and more
    Updated pandas dataframe code so columns were specified. Also went ahead and created a bug-free stub for the record creation function. Added a main method. Added a helper function to convert full team names into the names I'm using.

--- ML_Model/parser.py
+++ ML_Model/parser.py
@@ -6,16 +6,34 @@ team_names = ["buffalo", "49er", "eagles", "vikings", "bears", "jets", "giants",
               "lions", "packers", "buccaneers", "falcons", "panthers", "saints", "rams", "seahawks", "cardinals",
               "jaguars", "texans", "chiefs", "ravens"]
 
-print(len(team_names))
+
+logged_games = {}
+
+def convert_full_name_to_acronym(full_name: str):
+    check = full_name.lower()
+    for name in team_names:
+        if name in check:
+            return name
 
 
 def load_datasheet(year: int, team: str, are_games):
     str_year = str(year)
     if are_games:
         file_name = f"{team}_{str_year}_games.csv"
+        game_record = pd.read_csv(file_name)
+        game_record.columns = ["week_num", 'day_of_week', 'date_str', 'time', 'missing_hyperlink', 'win/lose', 'went_ot', 'team_rec',
+                               'away_stat', 'opp_team', 'team_pt', 'opp_pt', 'num_fd', 'total_yards', 'pass_yards',
+                               'rush_yards', 'turnovers', 'def_fd', 'def_tot_yds', 'def_pass_yds', 'def_rush_yds',
+                               'def_to', 'exp_points', 'def_exp_points', 'spec_exp_points']
+        return game_record
     else:
         file_name = f"{team}_{str_year}.csv"
-    return pd.read_csv(file_name)
+        team_record = pd.read_csv(file_name)
+        team_record.columns = ["row_names", "PF", "Yds", "ply", "y/p", "to", "fl", "fst_down", "pcmp", "patt", "pyds",
+                               "ptd", "pint", 'pny/a', 'pfd', 'ratt', 'ryds', 'rtd', 'ry/a', 'rfstd', 'pen', 'penyds', 'pen_fst_down',
+                               '#dr', 'sc%', 'TO%', 'avg_start_pos', 'avg_time_dr', 'avg_num_plys', 'avg_dr_yds',
+                               'avg_dr_pts']
+        return team_record
 
 
 def create_records(team: str, year: int):
@@ -24,26 +42,30 @@ def create_records(team: str, year: int):
 
     records = pd.DataFrame()
 
-    for game in games.iterrows():
-        #TODO
-        print(game)
-        final = pd.DataFrame()
-        final.loc["Week"] = game[0]
+    for index, game in games.iterrows():
+        if game["week_num"] != "Week" or game['week_num'] is not None:
+            #TODO
+            opp_name = convert_full_name_to_acronym(game["opp_team"])
+            opp_stats = load_datasheet(year, opp_name, False)
+            opp_games = load_datasheet(year, opp_name, True)
+            final = pd.DataFrame()
+            final["Week"] = game["week_num"]
+
     return records
 
-def create_training_data():
-    data = pd.DataFrame()
 
-    for year in [2024]:
-        for team in team_names:
-            records = create_records(team, year)
-            if data.empty:
-                data = records
-            else:
-                pd.concat([data, records])
-    # data.to_csv("final_data")
+def create_training_data(year: int):
+    data = pd.DataFrame()
+    for team in team_names:
+        records = create_records(team, year)
+        if data.empty:
+            data = records
+        else:
+            pd.concat([data, records])
+    file_name = f"formatted_data_{year}"
+    #data.to_csv(file_name)
     print("Done")
 
 
-create_training_data()
-
+if __name__ == "__main__":
+    create_training_data(2024)
