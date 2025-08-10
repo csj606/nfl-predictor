5bf354a17e05283328a402b02278d391a8426527
csj606 <139289685+csj606@users.noreply.github.com>
2025-08-09 23:08:12 -0500

    Created week number lambda function
    Added function to parse the ESPN data to get the season week ranges

--- LambdaScripts/Schedule/schedule.py
+++ LambdaScripts/Schedule/schedule.py
@@ -1,8 +1,25 @@
 import requests
+import time
+import boto3
+
 
 def schedule():
-    espn_games = requests.get("")
-
+    espn_games = requests.get("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard")
+    if not espn_games.ok:
+        print("Request failed, retrying in five minutes")
+        time.sleep(300)
+        schedule()
+    data = espn_games.json()
+    nfl_data = data["leagues"][0]
+    regular_calendar = nfl_data["calendar"][1]
+    table = boto3.resource("dynamodb").Table('standings')
+    for week in regular_calendar:
+        table.put_item(
+            Item={
+                'date_range': f"{week['startDate']}{week['endDate']}",
+                'week_num': int(week['value'])
+            }
+        )
 
 
 if __name__ == "__main__":
