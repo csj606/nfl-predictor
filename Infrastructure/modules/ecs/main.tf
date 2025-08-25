e8b5234bbebd6bd84f8a1d01bf1a441a421718b2
csj606 <139289685+csj606@users.noreply.github.com>
2025-08-24 22:04:36 -0500

    Added tasks, task definitions
    Added ECS cluster, task definitions and stubs for some additional stuff related to the ECS. Added new modules for the ECR and CloudWatch components. Need to add a few more bits and bobs but cloud infrastructure is starting to be finalized.

--- Infrastructure/modules/ecs/main.tf
+++ Infrastructure/modules/ecs/main.tf
@@ -1,15 +1,58 @@
-resource "aws_iam_role" "ec2_role" {
-    name = "cluster"
-    assume_role_policy = jsonencode({
-        Version = "2025-08-20"
-        Effect = "Allow"
-        Sid = ""
-        Principal = {
-            Service = "ecs-tasks.amazonaws.com"
-        }
+resource "aws_ecs_cluster" "nfl_cluster"{
+    name = "blue_red"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "nfl_cluster_providers" {
+    cluster_name = aws_ecs_cluster.nfl_cluster.name
+    capacity_providers = ["FARGATE"]
+
+    default_capacity_provider_strategy {
+      capacity_provider = "FARGATE"
+      base = 1
+      weight = 100
+    }
+}
+
+resource "aws_ecs_service" "web_server"{
+    name = "web_server"
+    cluster = aws_ecs_cluster.nfl_cluster.id
+    task_definition = aws_ecs_task_definition.web_server_definition.arn
+    desired_count = 1
+    
+    launch_type = "FARGATE"
+    network_configuration {
+      subnets = [  ]
+      security_groups = [  ]
+      assign_public_ip = true
+    }
+}
+
+resource "aws_ecs_service" "redis" {
+    name = "redis"
+    cluster = aws_ecs_cluster.nfl_cluster.id
+    task_definition = aws_ecs_task_definition.redis_definition.arn
+    desired_count = 1
+
+    launch_type = "FARGATE"
+    network_configuration {
+      subnets = [  ]
+      security_groups = [  ]
+      assign_public_ip = true
+    }
+}
+
+resource "aws_ecs_task_definition" "web_server_definition" {
+    family = "service"
+    requires_compatibilities = [ "FARGATE" ]
+    container_definitions = jsonencode({
+
     })
 }
 
-resource "aws_ecs_cluster" "nfl_cluster"{
-    name = "blue_red"
+resource "aws_ecs_task_definition" "redis_definition" {
+    family = "service"
+    requires_compatibilities = ["FARGATE"]
+    container_definitions = jsonencode({
+
+    })
 }
