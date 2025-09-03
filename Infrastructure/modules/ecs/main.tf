bb206e28771f5add717e44f7b4187e0dcc18b48b
csj606 <139289685+csj606@users.noreply.github.com>
2025-09-03 00:21:49 -0500

    Security groups actually added
    Forgot to save the file...

--- Infrastructure/modules/ecs/main.tf
+++ Infrastructure/modules/ecs/main.tf
@@ -147,3 +147,53 @@ resource "aws_cloudwatch_log_group" "ecs_group" {
         Project = "nfl"
     }
 }
+
+resource "aws_security_group" "web_server_sg" {
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "redis_sg"{
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = 6379
+        to_port = 6379
+        protocol = "tcp"
+        security_groups = [aws_security_group.web_server_sg.id]
+    }
+
+    egress {
+        from_port = 6379
+        to_port = 6379
+        protocol = "tcp"
+        cidr_blocks = [aws_security_group.web_server_sg.id]
+    }
+}
