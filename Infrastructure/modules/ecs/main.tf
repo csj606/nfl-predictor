946285227fa85bbb0681bcdb6f6d19a75a62e542
csj606 <139289685+csj606@users.noreply.github.com>
2025-09-02 22:10:51 -0500

    Finished Infra!
    Added security groups, logging to the ECS tasks. All cloud infra should be complete at this point... minus bugs of course.

--- Infrastructure/modules/ecs/main.tf
+++ Infrastructure/modules/ecs/main.tf
@@ -114,7 +114,36 @@ resource "aws_iam_role" "ecs_execution_role"{
     })
 }
 
+resource "aws_iam_policy" "ecs_logs_policy"{
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents"
+                ]
+                Resource = "${aws_cloudwatch_log_group.ecs_group.arn}:*"
+            }
+        ]
+    })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
     role = aws_iam_role.ecs_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_log_attach" {
+    role = aws_iam_role.ecs_execution_role.name
+    policy_arn = aws_iam_policy.ecs_logs_policy.arn
+}
+
+resource "aws_cloudwatch_log_group" "ecs_group" {
+    name = "/ecs"
+    retention_in_days = 7
+    tags = {
+        Project = "nfl"
+    }
 }
